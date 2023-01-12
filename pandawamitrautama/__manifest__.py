9b3803a5e1490bfc2284c5b07f57571dde78410f
dudirudiana <dudeeebzr@gmail.com>
2023-01-12 08:25:16 +0000

    local

--- pandawamitrautama/__manifest__.py
+++ pandawamitrautama/__manifest__.py
@@ -5,5 +5,9 @@
     'version': '1.0.0',
     'depends': ['base', 'base_setup', 'purchase'],
     'license': 'LGPL-3', # pilih antara GPL-2, GPL-3, AGPL-3, LGPL-3, OEEL-1, OPL-1, atau Other proprietary
-    'installable': True
+    'installable': True,
+    'data': [
+        'views/purchase.xml',
+        'views/partner.xml'
+    ],
 }
